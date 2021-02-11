0e3a69a02da2c6069eaf832ab18d4b81fa782ee7
anuar2k <anuar2k@outlook.com>
2021-02-11 17:09:54 +0100

    proj02 258

--- proj02/refinement.cpp
+++ proj02/refinement.cpp
@@ -35,25 +35,27 @@ class Subset {
     Subset* next;
     int first_vtx;
     int last_vtx;
-    int last_part_epoch = -1;
+    int last_part_epoch = -1; //tracks if this subset was partitioned in current partition process
 };
 
 struct Vertex {
     const int vtx;
     Subset* subset;
-    int vtx_map_idx;
+    int vtx_map_idx; //index of the vertex in vertices_map
 };
 
-std::vector<int> lexbfs_order(std::vector<std::unordered_set<int>>& graph) {
+int chordal_maxclique(std::vector<std::unordered_set<int>>& graph) {
     int n = graph.size();
 
-    std::vector<Vertex> vertices;
-    std::vector<Vertex*> vertices_map;
+    std::vector<Vertex> vertices; //allocates Vertices, allows O(1) access by absolute index
+    std::vector<Vertex*> vertices_map; //allows O(1) access in reordered subsets
+
+    //reserve n, to make sure that pointers to vertices' elements will not be invalidated during push_backs
     vertices.reserve(n);
     vertices_map.reserve(n);
 
+    //add all vertices to the initial subset
     Subset* initial_subset = new Subset(nullptr, nullptr, 0, n - 1);
-
     for (int vtx = 0; vtx < n; vtx++) {
         vertices.emplace_back(Vertex {
             .vtx = vtx,
@@ -63,51 +65,56 @@ std::vector<int> lexbfs_order(std::vector<std::unordered_set<int>>& graph) {
         vertices_map.push_back(&vertices[vtx]);
     }
 
+    int maxclique = 1;
     for (int i = 0; i < n - 1; i++) {
+        //select the vertex from the leftmost subset
         int curr_vtx = vertices_map[i]->vtx;
 
         //remove vertex from subset and remove the subset, if it's empty
         vertices_map[i]->subset->first_vtx += 1;
-
         if (vertices_map[i]->subset->empty()) {
             delete vertices_map[i]->subset;
         }
 
         vertices_map[i]->subset = nullptr;
 
-        //partition
+        int curr_clique = 1;
+        //subset partition
         for (int neighbor : graph[curr_vtx]) {
             Subset* neighbor_subset = vertices[neighbor].subset;
 
-            if (neighbor_subset != nullptr) {
-                //push the neighbor to the beggining of its subset
-                int old_subset_first_map_idx = neighbor_subset->first_vtx;
-                int old_neighbor_map_idx = vertices[neighbor].vtx_map_idx;
+            if (neighbor_subset == nullptr) {
+                curr_clique += 1;
+            }
+            else {
+                //push the neighbor to the beginning of its subset by swapping
+                int old_subset_first_idx = neighbor_subset->first_vtx;
+                int old_neighbor_idx = vertices[neighbor].vtx_map_idx;
 
                 std::swap(vertices[neighbor].vtx_map_idx, vertices_map[neighbor_subset->first_vtx]->vtx_map_idx);
-                std::swap(vertices_map[old_subset_first_map_idx], vertices_map[old_neighbor_map_idx]);
+                std::swap(vertices_map[old_subset_first_idx], vertices_map[old_neighbor_idx]);
 
                 neighbor_subset->first_vtx += 1;
 
                 //was this subset already partitioned in this iteration?
-                if (neighbor_subset->last_part_epoch == i) {
-                    //if yes, then move the element to previous subset
+                if (neighbor_subset->last_part_epoch == i) { //if yes, then move the element to previous subset
                     neighbor_subset->prev->last_vtx += 1;
 
                     vertices[neighbor].subset = neighbor_subset->prev;
 
-                    //if old subset is empty, remove
+                    //if old subset is empty, remove it
                     if (neighbor_subset->empty()) {
                         delete neighbor_subset;
                     }
                 }
-                else {
-                    //else create a new subset
+                else { //else create a new subset
+                    //note, that old subset was already partitioned in this iteration
                     neighbor_subset->last_part_epoch = i;
+
+                    //insert a new subset, and if the old one is empty, remove it
                     Subset* insert_after = neighbor_subset->prev;
                     Subset* insert_before;
 
-                    //if old subset is empty, remove
                     if (neighbor_subset->empty()) {
                         insert_before = neighbor_subset->next;
                         delete neighbor_subset;
@@ -120,17 +127,15 @@ std::vector<int> lexbfs_order(std::vector<std::unordered_set<int>>& graph) {
                 }
             }
         }
-    }
 
-    std::vector<int> result;
-    result.reserve(n);
-    for (int i = 0; i < n; i++) {
-        result.push_back(vertices_map[i]->vtx);
+        if (curr_clique > maxclique) {
+            maxclique = curr_clique;
+        }
     }
 
     delete vertices_map[n - 1]->subset;
 
-    return result;
+    return maxclique;
 }
 
 int maxclique(std::vector<std::unordered_set<int>>& graph, std::vector<int>& peo) {
@@ -182,7 +187,6 @@ int main() {
             graph[b].insert(a);
         }
 
-        std::vector<int> order = lexbfs_order(graph);
-        std::cout << std::max(2, maxclique(graph, order) - 1) << std::endl;
+        std::cout << std::max(2, chordal_maxclique(graph) - 1) << std::endl;
     }
 }
